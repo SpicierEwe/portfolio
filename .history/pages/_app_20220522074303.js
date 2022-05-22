d6dd4761917386736e1c600c84a679227701f175
SpicierEwe <83544142+SpicierEwe@users.noreply.github.com>
2022-05-22 07:53:57 +0530

    commit 2.0.6

---
+++ .history/pages/_app_20220522074303.js
@@ -0,0 +1,15 @@
+import MobileNavBar from "../components/navBar/mobile_nav/mobile_nav_bar";
+import NavBar from "../components/navBar/nav_bar";
+import { MobileNavContextProvider } from "../providers/context_provider";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <MobileNavContextProvider>
+      <Component {...pageProps} />
+      <NavBar></NavBar>
+    </MobileNavContextProvider>
+  );
+}
+
+export default MyApp;
---
+++ .history/pages/_app_20220522074326.js
@@ -0,0 +1,15 @@
+import MobileNavBar from "../components/navBar/mobile_nav/mobile_nav_bar";
+import NavBar from "../components/navBar/nav_bar";
+import { MobileNavContextProvider } from "../providers/context_provider";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <MobileNavContextProvider>
+      <NavBar></NavBar>
+      <Component {...pageProps} />
+    </MobileNavContextProvider>
+  );
+}
+
+export default MyApp;
